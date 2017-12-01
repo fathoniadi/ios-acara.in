e6e80976756eb7f11ef2cd3c5f3947599c0af1cb
fathoniadi <fathoni.adi@gmail.com>
2017-12-01 19:32:08 +0700

    udah bisa tampil map di add acara

--- acara.in/Acara/AddAcaraController.swift
+++ acara.in/Acara/AddAcaraController.swift
@@ -1,43 +1,46 @@
 //
-//  HomeController.swift
+//  AddAcaraController.swift
 //  acara.in
 //
-//  Created by Fathoni Adi Kurniawan on 27/11/17.
+//  Created by Fathoni Adi Kurniawan on 28/11/17.
 //  Copyright © 2017 Fathoni Adi Kurniawan. All rights reserved.
 //
 
 import UIKit
 import GoogleMaps
 import GooglePlaces
+class AddAcaraController: UIViewController, CLLocationManagerDelegate {
 
-class HomeController: UIViewController, CLLocationManagerDelegate {
+ 
 
-    @IBAction func tambah_acara_button(_ sender: UIBarButtonItem) {
-        self.performSegue(withIdentifier: "add_acara_sogue", sender: self)
-    }
+    //@IBOutlet var viewer2: GMSMapView!
+    @IBOutlet fileprivate var viewer2: GMSMapView!
     var locationManager = CLLocationManager()
-    lazy var mapView = GMSMapView()
+    var mapView = GMSMapView()
     
-    @IBAction func menu_button(_ sender: UIBarButtonItem) {
-        self.performSegue(withIdentifier: "menu_sogue", sender: self)
-    }
+   // @IBOutlet weak var add_acara_nav: UINavigationItem!
+
+
     override func viewDidLoad() {
         super.viewDidLoad()
         
         let camera = GMSCameraPosition.camera(withLatitude: -33.86, longitude: 151.20, zoom: 13.0)
         self.mapView = GMSMapView.map(withFrame: CGRect.zero, camera: camera)
         
-        view = self.mapView
+        //view.addSubview(viewer2)
+        //viewer2 = self.mapView
+        viewer2.camera = camera
         
-        self.mapView.isMyLocationEnabled = true
-        self.mapView.settings.myLocationButton = true
+        viewer2.isMyLocationEnabled = true
+        viewer2.settings.myLocationButton = true
         
         self.locationManager.delegate = self
         self.locationManager.requestWhenInUseAuthorization()
         self.locationManager.desiredAccuracy = kCLLocationAccuracyBest
         self.locationManager.startUpdatingLocation()
         
-
+        
+        
         // Do any additional setup after loading the view.
     }
     
@@ -45,28 +48,21 @@ class HomeController: UIViewController, CLLocationManagerDelegate {
         let userLocation = locations.last
         let camera = GMSCameraPosition.camera(withLatitude: userLocation!.coordinate.latitude,
                                               longitude: userLocation!.coordinate.longitude, zoom: 13.0)
-        self.mapView.animate(to: camera)
+        viewer2.camera = camera
+        viewer2.animate(to: camera)
         self.locationManager.stopUpdatingLocation()
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
     
     
-
+    
     @IBAction func returned(segue: UIStoryboardSegue) {
-       
+        
     }
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
+    
 
 }
