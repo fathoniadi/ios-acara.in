24c416f21166b01f6b185872187c909b31f7533e
fathoniadi <fathoni.adi@gmail.com>
2017-12-01 21:51:46 +0700

    bisa search

--- acara.in/Acara/AddAcaraController.swift
+++ acara.in/Acara/AddAcaraController.swift
@@ -9,14 +9,15 @@
 import UIKit
 import GoogleMaps
 import GooglePlaces
-class AddAcaraController: UIViewController, CLLocationManagerDelegate {
-
- 
+class AddAcaraController: UIViewController, CLLocationManagerDelegate, GMSAutocompleteViewControllerDelegate, GMSMapViewDelegate {
+    var locationManager = CLLocationManager()
+    var mapView = GMSMapView()
+    
+    var marker = GMSMarker()
 
     //@IBOutlet var viewer2: GMSMapView!
     @IBOutlet fileprivate var viewer2: GMSMapView!
-    var locationManager = CLLocationManager()
-    var mapView = GMSMapView()
+    
     
    // @IBOutlet weak var add_acara_nav: UINavigationItem!
 
@@ -25,14 +26,11 @@ class AddAcaraController: UIViewController, CLLocationManagerDelegate {
         super.viewDidLoad()
         
         let camera = GMSCameraPosition.camera(withLatitude: -33.86, longitude: 151.20, zoom: 13.0)
-        self.mapView = GMSMapView.map(withFrame: CGRect.zero, camera: camera)
-        
-        //view.addSubview(viewer2)
-        //viewer2 = self.mapView
         viewer2.camera = camera
         
         viewer2.isMyLocationEnabled = true
         viewer2.settings.myLocationButton = true
+        viewer2.delegate = true
         
         self.locationManager.delegate = self
         self.locationManager.requestWhenInUseAuthorization()
@@ -44,10 +42,15 @@ class AddAcaraController: UIViewController, CLLocationManagerDelegate {
         // Do any additional setup after loading the view.
     }
     
+    
     func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
         let userLocation = locations.last
         let camera = GMSCameraPosition.camera(withLatitude: userLocation!.coordinate.latitude,
                                               longitude: userLocation!.coordinate.longitude, zoom: 13.0)
+        
+        marker.position = CLLocationCoordinate2D(latitude: userLocation!.coordinate.latitude, longitude: userLocation!.coordinate.longitude)
+        marker.map = viewer2
+        
         viewer2.camera = camera
         viewer2.animate(to: camera)
         self.locationManager.stopUpdatingLocation()
@@ -59,10 +62,46 @@ class AddAcaraController: UIViewController, CLLocationManagerDelegate {
     }
     
     
-    
     @IBAction func returned(segue: UIStoryboardSegue) {
         
     }
     
+    func viewController(_ viewController: GMSAutocompleteViewController, didAutocompleteWith place: GMSPlace) {
+        let camera = GMSCameraPosition.camera(withLatitude: place.coordinate.latitude, longitude: place.coordinate.longitude, zoom: 15.0)
+        
+        viewer2.camera = camera
+        self.dismiss(animated: true, completion: nil) // dismiss after select place
+    }
+    
+    func viewController(_ viewController: GMSAutocompleteViewController, didFailAutocompleteWithError error: Error) {
+        
+    }
+    
+    func wasCancelled(_ viewController: GMSAutocompleteViewController) {
+        self.dismiss(animated: true, completion: nil) // when cancel search
+    }
+    
+    @IBAction func searchButton(_ sender: UIBarButtonItem) {
+        let autoCompleteController = GMSAutocompleteViewController()
+        autoCompleteController.delegate = self
+        
+        self.locationManager.startUpdatingLocation()
+        self.present(autoCompleteController, animated: true, completion: nil)
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        self.navigationController?.setNavigationBarHidden(false, animated: animated);
+        super.viewWillDisappear(animated)
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        self.navigationController?.setNavigationBarHidden(false, animated: animated)
+    }
+    
+    func mapView(_ mapView: GMSMapView, didTap marker: GMSMarker) -> Bool {
+        print ("MarkerTapped Locations: \(marker.position.latitude), \(marker.position.longitude)")
+        return true
+    }
 
 }
